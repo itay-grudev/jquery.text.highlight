8cd5cb4ab9b7f36377722da9196afadf3d2fd1df
Itay Grudev <itay@grudev.com>
2016-01-22 05:52:37 +0000

    Multiple changes, improvments and bugfixes. Refactored code

--- jquery.text.highlight.js
+++ jquery.text.highlight.js
@@ -28,101 +28,79 @@
 // @compilation_level ADVANCED_OPTIMIZATIONS
 // ==/ClosureCompiler==
 (function ( $, undefined ) {
-
-  // The actual plugin constructor
-  function Plugin( element, term, options ) {
-    this.element = element;
-    this.options = $['extend']( {}, {
+  // Apply highlight
+  $['fn']['textHighlight'] = function ( term, options ) {
+    // Default options
+    options = $['extend']( {}, {
       'element': 'mark',
       'class': '',
       'caseSensitive': false
     }, options);
-    this.highlight( term );
-  }
 
-  Plugin.prototype.highlight = function ( term ) {
-    if( term instanceof RegExp || typeof term == 'string' ) {
-      // The term is a regular expression or a string
-      this.processNode( term, this.element );
-    } else if( typeof term == 'array' ||
-               typeof term == 'object' && term.length ) {
-      // The term is an array containing any of the two above
-      for( var i = 0; i < term.length; ++i ) {
-        this.processNode( term[i], this.element );
+    function processNode( term, node ) {
+      if( node.nodeType == Node.TEXT_NODE ) {
+        var pos = null, length;
+        if( term instanceof RegExp ) {
+          // If the ignoreCase of the RegEx has incorrect setting
+          if( term.ignoreCase == options['caseSensitive'] ) {
+            var flags = (options['caseSensitive'])? '' : 'i';
+            flags = term['flags'].replace('i', '') + flags;
+            term = new RegExp( term.source, flags );
+          }
+          var match = term.exec( node.data );
+          if( match !== null ) {
+            pos = match.index;
+            length = match[0].length;
+          }
+        } else if( typeof term == 'string' ) {
+          if( term.length == 0 ) return 0;
+          if( options['caseSensitive'] )
+            pos = node.data.indexOf( term );
+          else pos = node.data.toUpperCase().indexOf( term.toUpperCase() );
+          length = term.length;
+        }
+        if( pos !== null && pos !== -1 ) {
+          var highlightedNode = document.createElement( options['element'] );
+          highlightedNode.className = options['class'];
+          $(highlightedNode)['attr']( 'data-text-highlight', '' );
+          var beginingNode = node.splitText( pos );
+          var endNode = beginingNode.splitText( length );
+          var middleNode = beginingNode.cloneNode( true );
+          highlightedNode.appendChild( middleNode );
+          beginingNode.parentNode.replaceChild( highlightedNode, beginingNode );
+          return 1;
+        }
+      } else if( node.nodeType == Node.ELEMENT_NODE && node.childNodes &&
+                 ! /(script|style)/i.test( node.tagName ) &&
+                 ! node.hasAttribute('data-text-highlight') ) {
+        for( var i = 0; i < node.childNodes.length; ++i ) {
+          i += processNode( term, node.childNodes[i] );
+        }
       }
-    }
-  }
 
-  Plugin.prototype.processNode = function( term, node ) {
-    if( node.nodeType == Node.TEXT_NODE ) {
-      var pos = null, length;
-      if( term instanceof RegExp ) {
-        // If the ignoreCase of the RegEx has incorrect setting
-        if( term.ignoreCase == this.options['caseSensitive'] ) {
-          var flags = (this.options['caseSensitive'])? '' : 'i';
-          flags = term['flags'].replace('i', '') + flags;
-          term = new RegExp( term.source, flags );
-        }
-        var match = term.exec( node.data );
-        if( match !== null ) {
-          pos = match.index;
-          length = match[0].length;
-        }
-      } else if( typeof term == 'string' ) {
-        if( this.options['caseSensitive'] )
-          pos = node.data.indexOf( term );
-        else pos = node.data.toUpperCase().indexOf( term.toUpperCase() );
-        length = term.length;
-      }
-      if( pos !== null && pos > 0 ) {
-        var highlightedNode = document.createElement( this.options['element'] );
-        highlightedNode.className = this.options['class'];
-        $(highlightedNode)['attr']( 'data-text-highlight', '' );
-        var beginingNode = node.splitText( pos );
-        var endNode = beginingNode.splitText( length );
-        var middleNode = beginingNode.cloneNode( true );
-        highlightedNode.appendChild( middleNode );
-        beginingNode.parentNode.replaceChild( highlightedNode, beginingNode );
-        return 1;
-      }
-    } else if( node.nodeType == Node.ELEMENT_NODE && node.childNodes &&
-               ! /(script|style)/i.test( node.tagName ) ){
-      for( var i = 0; i < node.childNodes.length; ++i ) {
-        i += this.processNode( term, node.childNodes[i] );
-      }
+      return 0;
     }
 
-    return 0;
-  }
-
-  Plugin.prototype.remove = function () {
-    $(this.element)['find']('[data-text-highlight]')['each']( function() {
-      this.parentNode.replaceChild( this.firstChild, this );
-    });
-    this.element.normalize();
-  }
-
-  // A really lightweight plugin wrapper around the constructor,
-  // preventing against multiple instantiations
-  $['fn']['textHighlight'] = function ( term, options ) {
     return this['each'](function () {
-      if ( ! $['data'](this, 'plugin_textHighlight') ) {
-        $['data']( this, 'plugin_textHighlight', new Plugin( this, term, options ) );
-      } else {
-        $['data']( this, 'plugin_textHighlight' ).remove();
-        $['removeData']( this, 'plugin_textHighlight' );
-        $['data']( this, 'plugin_textHighlight', new Plugin( this, term, options ) );
+      if( term instanceof RegExp || typeof term == 'string' ) {
+        // The term is a regular expression or a string
+        processNode( term, this );
+      } else if( typeof term == 'object' && term.length !== undefined ) {
+        // The term is an array containing any of the two above
+        for( var i = 0; i < term.length; ++i ) {
+          processNode( term[i], this );
+        }
       }
     });
   }
 
-  // Remove highlight function that has access to the configuration from the
-  // highlight initialization function
+  // Remove highlight function
   $['fn']['removeHighlight'] = function ( options ) {
-    return this['each'](function () {
-      if ( ! $['data']( this, 'plugin_textHighlight' ) ) return;
-      $['data']( this, 'plugin_textHighlight' ).remove();
-      $['removeData']( this, 'plugin_textHighlight' );
+    return this['each']( function () {
+      $(this)['find']('[data-text-highlight]')['each']( function() {
+        this.parentNode.replaceChild( this.firstChild, this );
+      });
+      this.normalize();
     });
   }
 })( jQuery );
